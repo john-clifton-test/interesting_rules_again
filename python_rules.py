91d6aa4fc5bf073b879cde542a2338cbae82aec7
John-Clifton-SonarSource <119871437+John-Clifton-SonarSource@users.noreply.github.com>
2025-03-25 20:38:42 +0000

    Update python_rules.py
    Simplifying code to make it easier to read.

--- python_rules.py
+++ python_rules.py
@@ -24,7 +24,7 @@ def complicated_code(input):
     d=4
     counter = 1
 
-    if a in (a,b,c,d):  # adding to get a scan going
+    if a in (a,b,c,d): 
         input += c
         if a < b:
             input += b
